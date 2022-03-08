b625ed937811c0e9052e1cabd88ba91fddae230c
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2022-03-08 19:24:35 +0100

    100 timesteps max

--- gym_continuous_maze/__init__.py
+++ gym_continuous_maze/__init__.py
@@ -3,5 +3,5 @@ import gym
 gym.register(
     id="ContinuousMaze-v0",
     entry_point="gym_continuous_maze.gym_continuous_maze:ContinuousMaze",
-    max_episode_steps=200,
+    max_episode_steps=100,
 )
